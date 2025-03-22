50bc1e8640f1e509af1e0abdbe5896d63afc081c
Maryna Serhiichuk <marina.serhiichuk@gmail.com>
2025-03-22 20:39:50 +0200

    not-found mobile

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -4,36 +4,39 @@ import { Button, Col, Flex, Row, Typography } from "antd";
 import styled from "@emotion/styled"
 import Tag from '@/components/Icons/TagBlock'
 import Link from "next/link";
+import { responsiveSize } from "@/utils/responsiveSize";
 
 const Padding = styled(Col)`
     display: flex;
     justify-content: center;
     color: ${({ theme }: any) =>  theme?.token?.colors?.primary[60]};
-    padding: 120px;
+    ${responsiveSize('padding', 120, 80, 50)}
 `
 
 const Text = styled(Col)`
     line-height: .9;
-    font-size: 100px;
+    ${responsiveSize('font-size', 100, 70, 50)}
 `
 
 const Number = styled(Text)`
-    font-size: 200px;
     transform: translateY(-15px);
+    ${responsiveSize('font-size', 200, 140, 100)}
 `
 
 const Icon = styled(Col)`
     svg {
+        ${responsiveSize('width', 150, 100, 70)}
+        ${responsiveSize('height', 150, 100, 70)}
         fill: ${({ theme }: any) =>  theme?.token?.colors?.primary[60]};
     }
 `
 
 export default function NotFound() {
     return <Padding span={24}>
-        <Col span={10}>
+        <Col xs={24} sm={24} md={16} lg={12} xl={10}>
             <Flex vertical gap={60}>
                 <Flex vertical>
-                    <Flex gap={10} align={'center'} justify={'center'}>
+                    <Flex gap={6} align={'center'} justify={'center'}>
                         <Number>
                             4
                         </Number>
--- frontend/src/components/Warning/Warning.tsx
+++ frontend/src/components/Warning/Warning.tsx
@@ -7,7 +7,7 @@ export const Warning: FC = () => {
     const [warn, setWarn] = useState(true)
 
     return <Fragment>
-        <Col style={{ position: 'absolute', transform: 'translateY(-45px)', cursor: 'pointer' }} onClick={() => setWarn(true)}>
+        <Col style={{ position: 'absolute', transform: 'translateY(-45px)', cursor: 'pointer', width: '40%' }} onClick={() => setWarn(true)}>
             <Typography.Title level={5}>
                 Please note that this is not a real website. It has been created solely for portfolio purposes.
             </Typography.Title>
@@ -23,12 +23,14 @@ export const Warning: FC = () => {
             closable={false}
             footer={null}
         >
-            <Typography.Title level={4}>
-                Please note that this website is not real. It has been created solely for demonstration purposes and is intended only as a portfolio example. All information presented on this site is fictional and should not be interpreted as real.
-            </Typography.Title>
-            <Button block type={'primary'} onClick={() => setWarn(false)}>
-                I understand
-            </Button>
+            <Flex vertical gap={30}>
+                <Typography.Title level={4}>
+                    Please note that this website is not real. It has been created solely for demonstration purposes and is intended only as a portfolio example. All information presented on this site is fictional and should not be interpreted as real.
+                </Typography.Title>
+                <Button block type={'primary'} onClick={() => setWarn(false)}>
+                    I understand
+                </Button>
+            </Flex>
         </Modal>
 
     </Fragment>
